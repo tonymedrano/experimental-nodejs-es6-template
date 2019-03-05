8b351a31a431faa00da4245543421d7de01384f2
tonymedrano <tony.medrano.rodriquez@ing.com>
2019-03-05 08:36:53 +0100

    Adding the initial files

---
+++ app.mjs
@@ -0,0 +1,33 @@
+/*
+ * Filename: /Users/tonymedrano/Desktop/node-zeromq-kickstarter-master/app.mjs
+ * Path: /Users/tonymedrano/Desktop/node-zeromq-kickstarter-master
+ * Created Date: Thursday, January 24th 2019, 12:20:58 am
+ * Author: tonymedrano
+ * 
+ * Copyright (c) 2019 Tony Medrano
+ */
+
+
+import fetch from 'node-fetch'
+import {
+    Printer
+} from "./printer"
+
+const p = Printer.create('Arrays/Objects')
+const url = 'https://jsonplaceholder.typicode.com/todos'
+
+const loadItems = async (dir) => await fetch(dir, {
+    method: 'get'
+})
+
+loadItems(url).then(response => response.json()).then((items) => {
+    const list = []
+    items.forEach(item => {
+        if (!item.completed && item.id <= 100) {
+            list.push(`--> id: ${item.id} - ${item.title}\n`)
+        }
+    });
+    p.print(list.join(' '), 'warning')
+}).catch((err) => {
+    p.print(err, 'error')
+})
